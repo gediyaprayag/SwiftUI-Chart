0d74dc55c25e104c0f11b067d6167cfd729f5fe0
Prayag Gediya <prayag.work60@gmail.com>
2023-03-02 12:14:09 +0530

    Initial Commit

---
+++ SwiftUI Chart/SwiftUI_ChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChartApp.swift
+//  SwiftUI Chart
+//
+//  Created by Prayag Gediya on 02/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
